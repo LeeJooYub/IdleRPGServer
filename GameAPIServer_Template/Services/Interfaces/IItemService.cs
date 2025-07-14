8867e389e8aa9d0e398870bea3a4555250584c00
leejooyub <leerik@com2us.com>
2025-07-14 19:07:19 +0900

    feat : GameAPIServer 템플릿 베이스라인 추가

---
+++ GameAPIServer_Template/Services/Interfaces/IItemService.cs
@@ -0,0 +1,14 @@
+﻿using GameAPIServer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameAPIServer.Servicies.Interfaces;
+
+public interface IItemService
+{
+    public Task<(ErrorCode, List<RewardData>)> ReceiveOneGacha(int uid, int gachaKey);
+
+    public Task<ErrorCode> ReceiveReward(int uid, RewardData reward);
+  
+
+}
